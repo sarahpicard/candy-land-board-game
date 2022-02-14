0a128244b2f18944a6fa7c6115de386b5e73fd4e
sarahpicard <sarahannrey225@gmail.com>
2022-02-14 18:27:59 -0500

    change boardColors array structure

--- js/app.js
+++ js/app.js
@@ -25,12 +25,19 @@ resetBtn.addEventListener('click', init)
 document.getElementById('card-button').addEventListener('click', handleClick)
 
 //------------------------------------------------------//
-let green = '#6fcb6c'
-let yellow = '#ebe6a7'
-let blue = '#6f79b6'
-let purple = '#de82f1'
-let red = '#d86d6d'
-let orange = '#ef9f6d'
+let g1, g2, g3, g4, g5, g6, g7 = '#6fcb6c'
+let y1, y2, y3, y4, y5, y6, y7 = '#ebe6a7'
+let b1, b2, b3, b4, b5, b6, b7 = '#6f79b6'
+let p1, p2, p3, p4, p5, p6, p7 = '#de82f1'
+let r1, r2, r3, r4, r5, r6, r7 = '#d86d6d'
+let o1, o2, o3, o4, o5, o6, o7 = '#ef9f6d'
+
+// let green = '#6fcb6c'
+// let yellow = '#ebe6a7'
+// let blue = '#6f79b6'
+// let purple = '#de82f1'
+// let red = '#d86d6d'
+// let orange = '#ef9f6d'
 
 init()
 
@@ -39,7 +46,9 @@ function init() {
   winner = null
   playerLocation = boardSquares[0]
   message.innerText = `It's time for player ${turn === 1 ? 'One' : 'Two'} to choose a card!`
-  boardColors = [green, purple, red, yellow, orange, blue, green, purple, yellow, red, blue, purple, yellow, green, orange, red, blue, purple, yellow, green, orange, red, blue, purple, green, yellow, red, orange, green, yellow, purple, blue]
+  boardColors = ['g1', 'p1', 'r1', 'y1', 'o1', 'b1', 'g2', 'p2', 'y2', 'r2', 'b2', 'p3', 'y3', 'g3', 'o2', 'r3', 'b3', 'p4', 'y4', 'g4', 'o3', 'r4', 'b4', 'p5', 'g5', 'y5', 'r5', 'o4', 'g6', 'y6', 'p6', 'b5']
+  // boardColors = [g1, p1, r1, y1, o1, b1, g2, p2, y2, r2, b2, p3, y3, g3, o2, r3, b3, p4, y4, g4, o3, r4, b4, p5, g5, y5, r5, o4, g6, y6, p6, b5]
+  // boardColors = [green, purple, red, yellow, orange, blue, green, purple, yellow, red, blue, purple, yellow, green, orange, red, blue, purple, yellow, green, orange, red, blue, purple, green, yellow, red orange, green, yellow, purple, blue]
   deck1 = ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'o1', 'o2', 'o3', 'o4', 'o5', 'o6', 'o7', 'p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7', 'y1', 'y2', 'y3', 'y4', 'y5', 'y6', 'y7']
   deck2 = []
   render()
@@ -78,10 +87,15 @@ function renderCards() {
 //----------------------------------------------------//
 
 function render() {
-  boardSquares.forEach((square, idx) => {
-    square.style.background = boardColors[idx]
-  })
-  
+  // boardSquares.forEach((square, idx) => {
+  //   square.style.background = boardColors[idx]
+  // })
+  for (let i = 0; i < boardColors.length; i++) {
+  if (boardColors == 'g1') {
+    square.style.background = '#6fcb6c'
+  }
+}
+
   if (!winner) {
     message.innerText = `It's time for player ${turn === 1 ? 'One' : 'Two'} to choose a card!`
   } else {
@@ -89,13 +103,21 @@ function render() {
   }
 }
 
-
 function cardToBoard(cardPicked) {
   console.log(cardPicked)
-  for (i = playerLocation; i < boardColors.length; i ++) {
-    if (cardPicked[0][0] === boardColors[i]) {
+  for (i = playerLocation; i < boardColors.length; i++) {
+    if (cardPicked[0] === boardColors[i]) {
+      movePlayer()
       return playerLocation = boardColors[i]
     }
+  }
+}
+
+function movePlayer() {
+  if (turn === 1) {
+    playerLocation.innerHTML = 'X'
+  } else {
+    playerLocation.innerHTML = 'O'
   }
 }
 
@@ -151,7 +173,3 @@ function cardToBoard(cardPicked) {
 // allow for both players to be on the same square 
 
 // use modal upon first entering the page for game directions (bootstrap)- STRETCH GOAL
-
-
-
-
