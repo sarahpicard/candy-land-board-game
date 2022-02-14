589220449a4f5b7c853b1fa5922243b9da13a28c
sarahpicard <sarahannrey225@gmail.com>
2022-02-14 14:10:08 -0500

    reconfigure render function

--- js/app.js
+++ js/app.js
@@ -6,9 +6,7 @@ let deck2 = []
 
 let cardToRemove, cardPicked
 
-
-
-// CACHED ELEMENT REFERENCES 
+//--------------------------------------------------------------------
 
 const message = document.querySelector('#message')
 const gameBoard = document.querySelector('#board')
@@ -21,108 +19,122 @@ let deck1El = document.getElementById('deck-1')
 let deck2El = document.getElementById('deck-2')
 
 
+//------------------------------------------------------------------------
 
-// EVENT LISTENERS   
-// click on restart button 
 resetBtn.addEventListener('click', init)
 
-boardSquares.forEach((square) => {
-  square.addEventListener('click', handleClickBoard)
-})
 
 document.getElementById('card-button').addEventListener('click', handleClick)
 
 
 //------------------------------------------------------//
+let green = '#6fcb6c'
+let yellow = '#ebe6a7'
+let blue = '#6f79b6'
+let purple = '#de82f1'
+let red = '#d86d6d'
+let orange = '#ef9f6d'
 
 init()
 
 function init() {
-  // player location = pre-square 1
   turn = 1
   winner = null
   playerLocation = boardSquares[0]
   message.innerText = `It's time for player ${turn === 1 ? 'One' : 'Two'} to choose a card!`
-  boardColors = ['green', 'purple', 'red', 'yellow', 'orange', 'blue', 'green', 'purple', 'yellow', 'red', 'blue', 'purple', 'yellow', 'green', 'orange', 'red', 'blue', 'purple', 'yellow', 'green', 'orange', 'red', 'blue', 'purple', 'green', 'yellow', 'red', 'orange', 'green', 'yellow', 'purple', 'blue']
-  greenSquares = []
-  purpleSquares = []
-  blueSquares = []
-  orangeSquares = []
-  redSquares = []
-  yellowSquares = []
+  boardColors = [green, purple, red, yellow, orange, blue, green, purple, yellow, red, blue, purple, yellow, green, orange, red, blue, purple, yellow, green, orange, red, blue, purple, green, yellow, red, orange, green, yellow, purple, blue]
   deck1 = ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'o1', 'o2', 'o3', 'o4', 'o5', 'o6', 'o7', 'p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7', 'y1', 'y2', 'y3', 'y4', 'y5', 'y6', 'y7']
   render()
 
 }
 
-// function playerCell(i) {
-//   let playerLocation = boardSquares[i]
-//   for (let i = 0; i < boardSquares.length; i++) {
-//     while (playerLocation.includes('green') 
-//   }
-// }
-
-// function playerCell(idx) {
-//   boardSquares.forEach(color, idx) {
-//     if (cardPicked === 'b1' || 'b2' || 'b3' || 'b4' || 'b5' || 'b6' || 'b7') {
-//       playerLocation[idx]
-//     }
-//   }
-// }
-
-
-
-// playerLocation - variable stores where play is currently (need one for both players?)- should store the index of players piece
-// next card - enter loop until you iterate to playerLocation === 'yellow'
-// that index becomes new playerLocation
-
-
 // STRETCH GOAL - modal at init for game directions 
 
+//----------------- CARD DECK FUNCTIONALITY ------------------------//
+function handleClick() {
+  if (deck1.length > 0) {
+    let randIdx = Math.floor(Math.random() * deck1.length)
+    cardPicked = deck1.splice(randIdx, 1)[0]
+    deck2.push(cardPicked)
+    renderCards(cardPicked)
+  }
+}
 
+function renderCards() {
+  deck2El.classList.remove('outline')
+  if (deck2.length > 1) {
+    deck2El.classList.remove(cardToRemove)
+  }
+  cardToRemove = cardPicked
+  deck2El.classList.add(cardPicked)
+  if (deck2.length === 26) {
+    deck2El.classList.add('shadow')
+    deck1El.classList.remove('shadow')
+  }
+  if (deck1.length === 0) {
+    deck1El.classList.add('outline')
+    deck1El.classList.remove('back')
+  }
+}
 //----------------------------------------------------//
-
-
-// RENDER FUNCTION 
-// function render() {
-//   greenSquares.forEach((cell, idx) => {
-//     let playerSpot
-//     if (greenSquares[idx] === 1) {
-//       playerSpot = 'X'
-//     } else if (greenSquares[idx] === -1) {
-//       playerSpot = 'O'
-//     } else if (greenSquares[idx] === null) {
-//       playerSpot = ''
-//     }
-//     boardSquares[idx].innerHTML === playerSpot
-//   })
-
-//   if (!winner) {
-//     message.innerText = `It's time for player ${turn === 1 ? 'One' : 'Two'} to choose a card!`
-//   } else {
-//     message.innerText = `Congratulations player ${winner === 1 ? 'One' : 'Two'}!`
-//   }
-// }
 
 function render() {
   boardSquares.forEach((square, idx) => {
-    console.log(square, idx)
     square.style.background = boardColors[idx]
   })
+
+  for (let i = 0; i < boardColors.length; i++) {
+    if (boardColors[i] === green) {
+      return playerSpot
+    } else {
+      if (boardColors[i] === red) {
+        return playerSpot
+      } else {
+        if (boardColors[i] === yellow) {
+          return playerSpot
+        } else {
+          if (boardColors[i] === orange) {
+            return playerSpot
+          } else {
+            if (boardColors[i] === purple) {
+              return playerSpot
+            } else {
+              if (boardColors[i] === blue) {
+                return playerSpot
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  if (!winner) {
+    message.innerText = `It's time for player ${turn === 1 ? 'One' : 'Two'} to choose a card!`
+  } else {
+    message.innerText = `Congratulations player ${winner === 1 ? 'One' : 'Two'}!`
+  }
+  cardToBoard()
 }
 
+
+function cardToBoard(card) {
+  console.log(card)
+  for (i = 0; i < deck1.length; i ++) {
+    if (cardPicked === 'b1') {
+      console.log('its blue')
+    }
+  }
+}
+
+function playerSpot() {
+
+}
 
 
 
 // use the index(s) of the iteration to access the corresponding color in the card deck
-// render message stating who's turn it is 
-// render win message if there is a win (congratulations!)
 
 
-// FINISH THIS FUNCTION //
-// connects to a click of each individual board square 
-function handleClickBoard(event) {
-}
 
 // ----------------------- IF USING HANDLECLICK ------//
 // ------ user would need to click on the spot corresponding to the card color
@@ -175,39 +187,5 @@ function handleClickBoard(event) {
 // use modal upon first entering the page for game directions (bootstrap)- STRETCH GOAL
 
 
-//----------------- CARD DECK FUNCTIONALITY ------------------------//
 
-
-// init()
-
-// function init() {
-//   deck1 = ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'o1', 'o2', 'o3', 'o4', 'o5', 'o6', 'o7', 'p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7', 'y1', 'y2', 'y3', 'y4', 'y5', 'y6', 'y7']
-// }
-
-function handleClick() {
-  if (deck1.length > 0) {
-    let randIdx = Math.floor(Math.random() * deck1.length)
-    cardPicked = deck1.splice(randIdx, 1)[0]
-    deck2.push(cardPicked)
-    render(cardPicked)
-  }
-}
-
-
-function renderCards() {
-  deck2El.classList.remove('outline')
-  if (deck2.length > 1) {
-    deck2El.classList.remove(cardToRemove)
-  }
-  cardToRemove = cardPicked
-  deck2El.classList.add(cardPicked)
-  if (deck2.length === 26) {
-    deck2El.classList.add('shadow')
-    deck1El.classList.remove('shadow')
-  }
-  if (deck1.length === 0) {
-    deck1El.classList.add('outline')
-    deck1El.classList.remove('back')
-  }
-}
 
